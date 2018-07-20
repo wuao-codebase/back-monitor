0951f9f0ab073613a2555fe3a92ed60198916957
fanhuimin.tp <987648551@qq.com>
2018-07-20 18:10:34 +0800

    7.20

--- src/test/java/top/watech/backmonitor/resttest/test.java
+++ src/test/java/top/watech/backmonitor/resttest/test.java
@@ -1,5 +1,6 @@
 package top.watech.backmonitor.resttest;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,4 +37,22 @@ public class test {
         multiValueMap.put("username","lake");//传值，但要在url上配置相应的参数
         restTemplate.getForObject("/test/get?username={username}",String.class,multiValueMap);
     }
+
+    @Test
+    public void testSSO(String username,String password){
+        String url = "http://portal-sso.wise-paas.com.cn/v1.3/auth/native";//请求地址
+        String param ="?username=" + username + "&password=" + password;
+        JSONObject json;
+        json = JSONObject.parseObject(param);
+        try{
+            Map<String,Object> map=new HashMap<>();
+            map.put("username","pataciot@aliyun.com");
+            map.put("password","P@ssw0rd");
+            String  str = restTemplate.postForObject(url+json,null, String.class,map);//所得结果为调整成String类型
+        }catch(Exception e){
+            System.out.println("登录失败");
+            e.printStackTrace();
+        }
+    }
+
 }
