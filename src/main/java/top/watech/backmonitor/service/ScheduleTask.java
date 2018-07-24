0f9aa6107e7db521c1d369cebbfa8143f4e6696f
wuao <130278228@qq.com>
2018-07-24 08:46:37 +0800

    RestFulAPI测试实例

---
+++ src/main/java/top/watech/backmonitor/service/ScheduleTask.java
@@ -0,0 +1,17 @@
+package top.watech.backmonitor.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by wuao.tp on 2018/7/23.
+ */
+@Component
+public class ScheduleTask  {
+    private  static int time =0;
+    @Scheduled(fixedRate = 1000)  //一秒
+    public void reportCurrentTime(){
+        time=(time+10)%60;
+        System.out.println(time);
+    }
+}
