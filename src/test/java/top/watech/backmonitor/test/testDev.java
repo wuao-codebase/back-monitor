c785fa721d77db6c8daa3884a74224d06ce3a671
tom_proglb <hello_glb@fhm.com>
2018-08-07 22:14:39 +0800

    SRP监控、设备监控

--- src/test/java/top/watech/backmonitor/test/testDev.java
+++ src/test/java/top/watech/backmonitor/test/testDev.java
@@ -2,6 +2,7 @@ package top.watech.backmonitor.test;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -57,6 +58,7 @@ public class testDev {
         return token;
     }
 
+    @Test
     public void testDev(){
         String url ="http://api-pataciot-acniotsense.wise-paas.com.cn/api/v1.0/device/group";
         HttpHeaders requestHeaders = new HttpHeaders();
@@ -66,7 +68,12 @@ public class testDev {
         HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<Map<String, Object>>(requestBody, requestHeaders);
         ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET,requestEntity,String.class);
         JSONObject resJsonObject = JSON.parseObject(responseEntity.getBody());//接口返回内容的json对象
-
+        System.err.println(resJsonObject);
+        //这里取到的resJsonObject对象，先取data中的内容get("data")
+        //再将data转成json数组，之后可以取出组id，组名
+        //循环组id
+        //将取到的id拼到 /device/detail?groupid=3&userid=1 下，生成新的URL
+        //然后取新接口的结果，判断linked
     }
 
 }
