8d2818bc30a90b5d853ef426a83a0cf784dc7bda
tom_proglb <hello_glb@fhm.com>
2018-08-08 20:19:05 +0800

    SRP监控、设备监控

--- src/test/java/top/watech/backmonitor/test/testMonitor.java
+++ src/test/java/top/watech/backmonitor/test/testMonitor.java
@@ -23,6 +23,7 @@ import java.util.Map;
 
 /**
  * Created by fhm on 2018/8/6.
+ * SRP监控逻辑
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -40,9 +41,9 @@ public class testMonitor {
     //accessToken
     public static String  accessToken;
 
-    //处理页面和接口类型（RestTemplete）
+    //处理接口类型（RestTemplete）
     //取结果与断言比对，最后结果为code，成功还是失败
-    public Integer testPageAndApi(MonitorItem monitorItem){
+    public Integer apiMonitor(MonitorItem monitorItem){
         String url=monitorItem.getUrl();
         Integer requestType=monitorItem.getRequestType();
         String asserts = monitorItem.getAsserts();
@@ -50,11 +51,8 @@ public class testMonitor {
         int code = 1;   //0为失败，1为成功
 
         HttpHeaders requestHeaders = new HttpHeaders();
-
         String requestBodyStr = monitorItem.getRequestBody();
-
         Map requestBody = new HashMap();
-
         ResponseEntity<String> responseEntity = null;
 
         //POST型，登录生成token
@@ -68,7 +66,7 @@ public class testMonitor {
 
             int statusCodeValue = responseEntity.getStatusCodeValue();
             if (statusCodeValue!=200){
-                //程序终止，退出
+                //登录一旦不成功，程序终止，退出
                 System.err.println("退出");
                 return null;
             }
@@ -118,13 +116,6 @@ public class testMonitor {
 
             JSONObject resJsonObject = JSON.parseObject(responseEntity.getBody());//接口返回内容的json对象
 
-            int statusCodeValue = responseEntity.getStatusCodeValue();
-            if (statusCodeValue!=200){
-                //程序终止，退出
-                System.err.println("退出");
-                return null;
-            }
-
             JSONArray assertsArraysJsonObject = JSON.parseArray(asserts);//传来的断言的json对象数组
 
             //断言数组循环判断
@@ -161,13 +152,48 @@ public class testMonitor {
     }
 
     //处理视频类型，最后结果是code，成功还是失败
-    public void testVideo(){
+    public void videoMonitor(MonitorItem monitorItem){
         //1、测试视频相关所有接口
         //2、测试视频文件获取
-
-
+        System.out.println(monitorItem.getMonitorName()+":"+"不知道");
     }
 
+    //处理页面类型，最后结果是code，成功还是失败
+    public void pageMonitor(MonitorItem monitorItem){
+        int code = 1;   //0为失败，1为成功
+
+        String url=monitorItem.getUrl();
+        HttpHeaders requestHeaders = new HttpHeaders();
+        Map requestBody = new HashMap();
+        if (monitorItem.getClassify()==2){
+            requestHeaders.add("Authorization","Bearer "+accessToken);
+        }
+        else if (monitorItem.getClassify()==4){
+            requestHeaders.add("Authorization","Bearer "+token);
+        }
+
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<Map<String, Object>>(requestBody, requestHeaders);
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+
+        int statusCodeValue = responseEntity.getStatusCodeValue();
+        if (statusCodeValue==200){
+            code=1;//成功
+            System.err.println(monitorItem.getMonitorName()+":"+"工作正常");
+            System.err.println("*******************************************");
+        }
+        else{
+            code=0;//失败
+            System.err.println(monitorItem.getMonitorName()+":"+"工作异常");
+            System.err.println("错误信息："+responseEntity.getBody());
+            System.err.println("*******************************************");
+        }
+    }
+
+    /**
+     * SRP监控流程
+     * 1、SSO登录。2、平台接口。3、SRP登录。4、SRP下接口。
+     */
     @Test
     public void monitorLogic(){
         System.err.println("start1");
@@ -179,37 +205,41 @@ public class testMonitor {
             //平台登录
             if (monitorItem.getClassify()==1){
 
-                testPageAndApi(monitorItem);
+                apiMonitor(monitorItem);
             }
             //平台接口
             else if (monitorItem.getClassify()==2){
                 //视频类型的监控项
                 if (monitorItem.getMonitorType()==2){
-                    //testVideo
-                    System.out.println(monitorItem.getMonitorName()+":"+"不知道");
-
+                    videoMonitor(monitorItem);
                 }
-                //页面或接口类型
-                else{
-                    testPageAndApi(monitorItem);
+                //接口类型
+                else if (monitorItem.getMonitorType()==1){
+                    apiMonitor(monitorItem);
+                }
+                //页面监控
+                else {
+                    pageMonitor(monitorItem);
                 }
             }
 
             //SRP登录
             if (monitorItem.getClassify()==3){
-                testPageAndApi(monitorItem);
+                apiMonitor(monitorItem);
             }
             //SRP接口
             else if (monitorItem.getClassify()==4){
                 //视频类型的监控项
                 if (monitorItem.getMonitorType()==2){
-                    //testVideo
-                    System.out.println(monitorItem.getMonitorName()+":"+"不知道");
-
+                    videoMonitor(monitorItem);
                 }
-                //页面或接口类型
-                else{
-                    testPageAndApi(monitorItem);
+                //接口类型
+                else if (monitorItem.getMonitorType()==1){
+                    apiMonitor(monitorItem);
+                }
+                //页面监控
+                else {
+                    pageMonitor(monitorItem);
                 }
             }
         }
